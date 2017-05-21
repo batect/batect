8fbc5c19d7ee806138fb1f6d7f3eb8c6d023116f
Charles Korn <me@charleskorn.com>
2017-05-21 21:09:04 +0200

    Add a second journey test for the scenario where no explicit command is given in the configuration file but one is given in the Dockerfile.

--- src/journeyTest/kotlin/decompose/journeytests/SimpleTaskJourneyTest.kt
+++ src/journeyTest/kotlin/decompose/journeytests/SimpleTaskJourneyTest.kt
@@ -15,32 +15,37 @@ import java.util.concurrent.TimeUnit
 import java.util.stream.Collectors
 
 object SimpleTaskJourneyTest : Spek({
-    given("a simple task") {
-        val testDirectory = Paths.get("src/journeyTest/resources/simple-task").toAbsolutePath()
-        val applicationPath = Paths.get("build/install/decompose-kt/bin/decompose-kt").toAbsolutePath()
+    mapOf(
+            "simple-task" to "a simple task with the command specified in the configuration file",
+            "simple-task-dockerfile-command" to "a simple task with the command specified in the Dockerfile"
+    ).forEach { testName, description ->
+        given(description) {
+            val testDirectory = Paths.get("src/journeyTest/resources/$testName").toAbsolutePath()
+            val applicationPath = Paths.get("build/install/decompose-kt/bin/decompose-kt").toAbsolutePath()
 
-        beforeGroup {
-            assert.that(Files.isDirectory(testDirectory), equalTo(true))
-            assert.that(Files.isExecutable(applicationPath), equalTo(true))
-        }
-
-        on("running that task") {
-            val process = ProcessBuilder(applicationPath.toString(), "decompose.yml", "the-task")
-                    .directory(testDirectory.toFile())
-                    .redirectErrorStream(true)
-                    .start()
-
-            process.waitFor(30, TimeUnit.SECONDS)
-
-            it("prints the output from that task") {
-                val outputReader = BufferedReader(InputStreamReader(process.getInputStream()))
-                val output = outputReader.lines().collect(Collectors.joining("\n"))
-
-                assert.that(output, containsSubstring("This is some output from the task"))
+            beforeGroup {
+                assert.that(Files.isDirectory(testDirectory), equalTo(true))
+                assert.that(Files.isExecutable(applicationPath), equalTo(true))
             }
 
-            it("returns the exit code from that task") {
-                assert.that(process.exitValue(), equalTo(123))
+            on("running that task") {
+                val process = ProcessBuilder(applicationPath.toString(), "decompose.yml", "the-task")
+                        .directory(testDirectory.toFile())
+                        .redirectErrorStream(true)
+                        .start()
+
+                process.waitFor(30, TimeUnit.SECONDS)
+
+                it("prints the output from that task") {
+                    val outputReader = BufferedReader(InputStreamReader(process.getInputStream()))
+                    val output = outputReader.lines().collect(Collectors.joining("\n"))
+
+                    assert.that(output, containsSubstring("This is some output from the task"))
+                }
+
+                it("returns the exit code from that task") {
+                    assert.that(process.exitValue(), equalTo(123))
+                }
             }
         }
     }
---
+++ src/journeyTest/resources/simple-task-dockerfile-command/build-env/Dockerfile
@@ -0,0 +1,3 @@
+FROM alpine:3.5
+
+CMD echo 'This is some output from the task' && exit 123

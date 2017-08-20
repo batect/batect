c4520583b75dfedde3f7a611ce6b0ef3b0435b7b
Charles Korn <me@charleskorn.com>
2017-08-20 11:52:34 +0200

    Add human-readable descriptions for steps.
    This makes debugging much, much easier.

--- src/main/kotlin/decompose/TaskStep.kt
+++ src/main/kotlin/decompose/TaskStep.kt
@@ -5,16 +5,50 @@ import decompose.docker.DockerContainer
 import decompose.docker.DockerImage
 import decompose.docker.DockerNetwork
 
-sealed class TaskStep
+sealed class TaskStep {
+    override fun toString() = this.javaClass.canonicalName
+}
+
 object BeginTaskStep : TaskStep()
-data class BuildImageStep(val container: Container) : TaskStep()
+
+data class BuildImageStep(val container: Container) : TaskStep() {
+    override fun toString() = super.toString() + "(container: ${container.name})"
+}
+
 object CreateTaskNetworkStep : TaskStep()
-data class CreateContainerStep(val container: Container, val image: DockerImage, val network: DockerNetwork) : TaskStep()
-data class RunContainerStep(val container: Container, val dockerContainer: DockerContainer) : TaskStep()
-data class StartContainerStep(val container: Container, val dockerContainer: DockerContainer) : TaskStep()
-data class StopContainerStep(val container: Container, val dockerContainer: DockerContainer) : TaskStep()
-data class RemoveContainerStep(val container: Container, val dockerContainer: DockerContainer) : TaskStep()
-data class WaitForContainerToBecomeHealthyStep(val container: Container, val dockerContainer: DockerContainer) : TaskStep()
-data class FinishTaskStep(val exitCode: Int) : TaskStep()
-data class DeleteTaskNetworkStep(val network: DockerNetwork) : TaskStep()
-data class DisplayTaskFailureStep(val message: String) : TaskStep()
+
+data class CreateContainerStep(val container: Container, val image: DockerImage, val network: DockerNetwork) : TaskStep() {
+    override fun toString() = super.toString() + "(container: ${container.name}, image ${image.id}, network: ${network.id}"
+}
+
+data class RunContainerStep(val container: Container, val dockerContainer: DockerContainer) : TaskStep() {
+    override fun toString() = super.toString() + "(container: ${container.name}, Docker container: ${dockerContainer.id})"
+}
+
+data class StartContainerStep(val container: Container, val dockerContainer: DockerContainer) : TaskStep() {
+    override fun toString() = super.toString() + "(container: ${container.name}, Docker container: ${dockerContainer.id})"
+}
+
+data class StopContainerStep(val container: Container, val dockerContainer: DockerContainer) : TaskStep() {
+    override fun toString() = super.toString() + "(container: ${container.name}, Docker container: ${dockerContainer.id})"
+}
+
+data class RemoveContainerStep(val container: Container, val dockerContainer: DockerContainer) : TaskStep() {
+    override fun toString() = super.toString() + "(container: ${container.name}, Docker container: ${dockerContainer.id})"
+}
+
+data class WaitForContainerToBecomeHealthyStep(val container: Container, val dockerContainer: DockerContainer) : TaskStep() {
+    override fun toString() = super.toString() + "(container: ${container.name}, Docker container: ${dockerContainer.id})"
+}
+
+data class FinishTaskStep(val exitCode: Int) : TaskStep() {
+    override fun toString() = super.toString() + "(exit code: $exitCode)"
+}
+
+data class DeleteTaskNetworkStep(val network: DockerNetwork) : TaskStep() {
+    override fun toString() = super.toString() + "(network: ${network.id})"
+}
+
+data class DisplayTaskFailureStep(val message: String) : TaskStep() {
+    override fun toString() = super.toString() + "(message: $message)"
+}

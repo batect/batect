a05462f1d3a928f3aa8e9a31d5a0ff0222680f4a
Charles Korn <me@charleskorn.com>
2017-09-19 17:12:57 +1000

    Check that curl is installed before attempting to use it.

--- wrapper/src/template.sh
+++ wrapper/src/template.sh
@@ -24,6 +24,11 @@ function haveVersionCachedLocally() {
 function download() {
     echo "Downloading batect version $VERSION from $DOWNLOAD_URL..."
 
+    if ! hash curl 2>/dev/null; then
+        echo "curl is not installed or not on your PATH. Please install it and try again." >&2
+        exit -1
+    fi
+
     mkdir -p "$CACHE_DIR"
     curl -# --fail --silent --show-error --output "$JAR_PATH" "$DOWNLOAD_URL"
 }

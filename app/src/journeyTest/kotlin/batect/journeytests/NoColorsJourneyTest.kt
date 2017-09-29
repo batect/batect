874db339ed6a8925326d57e12d44d4a8ba2ff9c8
Charles Korn <me@charleskorn.com>
2017-09-29 14:33:01 +1000

    Add journey tests for simple output and when colored output is disabled.

---
+++ app/src/journeyTest/kotlin/batect/journeytests/NoColorsJourneyTest.kt
@@ -0,0 +1,48 @@
+/*
+   Copyright 2017 Charles Korn.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package batect.journeytests
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.containsSubstring
+import com.natpryce.hamkrest.equalTo
+import com.natpryce.hamkrest.isEmpty
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+
+object NoColorsJourneyTest : Spek({
+    describe("when colored output is disabled") {
+        val runner = ApplicationRunner("simple-task")
+
+        on("running a task") {
+            val result = runner.runApplication(listOf("--no-colors", "run", "the-task"))
+
+            it("prints the output from that task") {
+                assertThat(result.output, containsSubstring("This is some output from the task\r\n"))
+            }
+
+            it("returns the exit code from that task") {
+                assertThat(result.exitCode, equalTo(123))
+            }
+
+            it("cleans up all containers it creates") {
+                assertThat(result.potentiallyOrphanedContainers, isEmpty)
+            }
+        }
+    }
+})

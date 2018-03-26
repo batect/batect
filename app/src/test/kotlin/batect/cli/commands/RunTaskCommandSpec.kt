8ec1721940f365a83cfbd24bb711f0690dcbae66
Charles Korn <me@charleskorn.com>
2018-03-26 22:02:27 +1100

    Refactor the tests for RunTaskCommand.

--- app/src/test/kotlin/batect/cli/commands/RunTaskCommandSpec.kt
+++ app/src/test/kotlin/batect/cli/commands/RunTaskCommandSpec.kt
@@ -86,208 +86,205 @@ object RunTaskCommandSpec : Spek({
                 }
             }
 
-            describe("when the configuration file can be loaded, Docker is available and the task has no dependencies") {
-                val taskExecutionOrderResolver = mock<TaskExecutionOrderResolver> {
-                    on { resolveExecutionOrder(config, taskName) } doReturn listOf(mainTask)
+            on("when the configuration file can be loaded") {
+                on("when Docker is available") {
+                    val dockerClient = mock<DockerClient> {
+                        on { checkIfDockerIsAvailable() } doReturn true
+                    }
+
+                    on("when the task has no dependencies") {
+                        val taskExecutionOrderResolver = mock<TaskExecutionOrderResolver> {
+                            on { resolveExecutionOrder(config, taskName) } doReturn listOf(mainTask)
+                        }
+
+                        on("and that task returns a non-zero exit code") {
+                            val taskRunner = mock<TaskRunner> {
+                                on { run(config, mainTask, runOptions) } doReturn expectedTaskExitCode
+                            }
+
+                            val command = RunTaskCommand(configFile, runOptions, configLoader, taskExecutionOrderResolver, taskRunner, updateNotifier, dockerClient, console, errorConsole, logger)
+                            val exitCode = command.run()
+
+                            it("runs the task") {
+                                verify(taskRunner).run(config, mainTask, runOptions)
+                            }
+
+                            it("returns the exit code of the task") {
+                                assertThat(exitCode, equalTo(expectedTaskExitCode))
+                            }
+
+                            it("does not print any blank lines after the task") {
+                                verify(console, never()).println()
+                            }
+
+                            it("displays any update notifications before running the task") {
+                                inOrder(taskRunner, updateNotifier) {
+                                    verify(updateNotifier).run()
+                                    verify(taskRunner).run(any(), any(), any())
+                                }
+                            }
+
+                            it("does not print anything to the error console") {
+                                verifyZeroInteractions(errorConsole)
+                            }
+                        }
+
+                        on("and that task returns a zero exit code") {
+                            val taskRunner = mock<TaskRunner> {
+                                on { run(config, mainTask, runOptions) } doReturn 0
+                            }
+
+                            val command = RunTaskCommand(configFile, runOptions, configLoader, taskExecutionOrderResolver, taskRunner, updateNotifier, dockerClient, console, errorConsole, logger)
+                            val exitCode = command.run()
+
+                            it("runs the task") {
+                                verify(taskRunner).run(config, mainTask, runOptions)
+                            }
+
+                            it("returns the exit code of the task") {
+                                assertThat(exitCode, equalTo(0))
+                            }
+
+                            it("does not print any blank lines after the task") {
+                                verify(console, never()).println()
+                            }
+
+                            it("displays any update notifications before running the task") {
+                                inOrder(taskRunner, updateNotifier) {
+                                    verify(updateNotifier).run()
+                                    verify(taskRunner).run(any(), any(), any())
+                                }
+                            }
+
+                            it("does not print anything to the error console") {
+                                verifyZeroInteractions(errorConsole)
+                            }
+                        }
+                    }
+
+                    on("when the task has a dependency") {
+                        val otherTask = Task("other-task", TaskRunConfiguration("the-other-container"))
+
+                        val taskExecutionOrderResolver = mock<TaskExecutionOrderResolver> {
+                            on { resolveExecutionOrder(config, taskName) } doReturn listOf(otherTask, mainTask)
+                        }
+
+                        on("and the dependency finishes with an exit code of 0") {
+                            val taskRunner = mock<TaskRunner> {
+                                on { run(config, otherTask, runOptions) } doReturn 0
+                                on { run(config, mainTask, runOptions) } doReturn expectedTaskExitCode
+                            }
+
+                            val command = RunTaskCommand(configFile, runOptions, configLoader, taskExecutionOrderResolver, taskRunner, updateNotifier, dockerClient, console, errorConsole, logger)
+                            val exitCode = command.run()
+
+                            it("runs the dependency task") {
+                                verify(taskRunner).run(config, otherTask, runOptions)
+                            }
+
+                            it("runs the main task") {
+                                verify(taskRunner).run(config, mainTask, runOptions)
+                            }
+
+                            it("runs the dependency before the main task, and prints a blank line in between") {
+                                inOrder(taskRunner, console) {
+                                    verify(taskRunner).run(config, otherTask, runOptions)
+                                    verify(console).println()
+                                    verify(taskRunner).run(config, mainTask, runOptions)
+                                }
+                            }
+
+                            it("returns the exit code of the main task") {
+                                assertThat(exitCode, equalTo(expectedTaskExitCode))
+                            }
+
+                            it("displays any update notifications before running the task") {
+                                inOrder(taskRunner, updateNotifier) {
+                                    verify(updateNotifier).run()
+                                    verify(taskRunner, atLeastOnce()).run(any(), any(), any())
+                                }
+                            }
+
+                            it("does not print anything to the error console") {
+                                verifyZeroInteractions(errorConsole)
+                            }
+                        }
+
+                        on("and the dependency finishes with a non-zero exit code") {
+                            val taskRunner = mock<TaskRunner> {
+                                on { run(config, otherTask, runOptions) } doReturn 1
+                            }
+
+                            val command = RunTaskCommand(configFile, runOptions, configLoader, taskExecutionOrderResolver, taskRunner, updateNotifier, dockerClient, console, errorConsole, logger)
+                            val exitCode = command.run()
+
+                            it("runs the dependency task") {
+                                verify(taskRunner).run(config, otherTask, runOptions)
+                            }
+
+                            it("does not run the main task") {
+                                verify(taskRunner, never()).run(config, mainTask, runOptions)
+                            }
+
+                            it("returns the exit code of the dependency task") {
+                                assertThat(exitCode, equalTo(1))
+                            }
+
+                            it("displays any update notifications before running the task") {
+                                inOrder(taskRunner, updateNotifier) {
+                                    verify(updateNotifier).run()
+                                    verify(taskRunner).run(any(), any(), any())
+                                }
+                            }
+
+                            it("does not print anything to the error console") {
+                                verifyZeroInteractions(errorConsole)
+                            }
+                        }
+                    }
+
+                    on("when determining the task execution order fails") {
+                        val exception = TaskExecutionOrderResolutionException("Something went wrong.")
+                        val taskExecutionOrderResolver = mock<TaskExecutionOrderResolver> {
+                            on { resolveExecutionOrder(config, taskName) } doThrow exception
+                        }
+
+                        val taskRunner = mock<TaskRunner>()
+
+                        val command = RunTaskCommand(configFile, runOptions, configLoader, taskExecutionOrderResolver, taskRunner, updateNotifier, dockerClient, console, errorConsole, logger)
+                        val exitCode = command.run()
+
+                        it("prints a message to the output") {
+                            verify(redErrorConsole).println("Something went wrong.")
+                        }
+
+                        it("returns a non-zero exit code") {
+                            assertThat(exitCode, !equalTo(0))
+                        }
+
+                        it("logs a message with the exception") {
+                            assertThat(logSink, hasMessage(withSeverity(Severity.Error) and withException(exception)))
+                        }
+                    }
                 }
 
-                val dockerClient = mock<DockerClient> {
-                    on { checkIfDockerIsAvailable() } doReturn true
-                }
-
-                on("and that task returns a non-zero exit code") {
-                    val taskRunner = mock<TaskRunner> {
-                        on { run(config, mainTask, runOptions) } doReturn expectedTaskExitCode
+                on("when Docker is not available") {
+                    val taskExecutionOrderResolver = mock<TaskExecutionOrderResolver>()
+                    val taskRunner = mock<TaskRunner>()
+                    val dockerClient = mock<DockerClient> {
+                        on { checkIfDockerIsAvailable() } doReturn false
                     }
 
                     val command = RunTaskCommand(configFile, runOptions, configLoader, taskExecutionOrderResolver, taskRunner, updateNotifier, dockerClient, console, errorConsole, logger)
                     val exitCode = command.run()
 
-                    it("runs the task") {
-                        verify(taskRunner).run(config, mainTask, runOptions)
+                    it("prints a message to the output") {
+                        verify(redErrorConsole).println("Docker is not installed, or the Docker executable is not on your PATH.")
                     }
 
-                    it("returns the exit code of the task") {
-                        assertThat(exitCode, equalTo(expectedTaskExitCode))
+                    it("returns a non-zero exit code") {
+                        assertThat(exitCode, !equalTo(0))
                     }
-
-                    it("does not print any blank lines after the task") {
-                        verify(console, never()).println()
-                    }
-
-                    it("displays any update notifications before running the task") {
-                        inOrder(taskRunner, updateNotifier) {
-                            verify(updateNotifier).run()
-                            verify(taskRunner).run(any(), any(), any())
-                        }
-                    }
-
-                    it("does not print anything to the error console") {
-                        verifyZeroInteractions(errorConsole)
-                    }
-                }
-
-                on("and that task returns a zero exit code") {
-                    val taskRunner = mock<TaskRunner> {
-                        on { run(config, mainTask, runOptions) } doReturn 0
-                    }
-
-                    val command = RunTaskCommand(configFile, runOptions, configLoader, taskExecutionOrderResolver, taskRunner, updateNotifier, dockerClient, console, errorConsole, logger)
-                    val exitCode = command.run()
-
-                    it("runs the task") {
-                        verify(taskRunner).run(config, mainTask, runOptions)
-                    }
-
-                    it("returns the exit code of the task") {
-                        assertThat(exitCode, equalTo(0))
-                    }
-
-                    it("does not print any blank lines after the task") {
-                        verify(console, never()).println()
-                    }
-
-                    it("displays any update notifications before running the task") {
-                        inOrder(taskRunner, updateNotifier) {
-                            verify(updateNotifier).run()
-                            verify(taskRunner).run(any(), any(), any())
-                        }
-                    }
-
-                    it("does not print anything to the error console") {
-                        verifyZeroInteractions(errorConsole)
-                    }
-                }
-            }
-
-            describe("when the configuration file can be loaded, Docker is available and the task has a dependency") {
-                val otherTask = Task("other-task", TaskRunConfiguration("the-other-container"))
-
-                val taskExecutionOrderResolver = mock<TaskExecutionOrderResolver> {
-                    on { resolveExecutionOrder(config, taskName) } doReturn listOf(otherTask, mainTask)
-                }
-
-                val dockerClient = mock<DockerClient> {
-                    on { checkIfDockerIsAvailable() } doReturn true
-                }
-
-                on("and the dependency finishes with an exit code of 0") {
-                    val taskRunner = mock<TaskRunner> {
-                        on { run(config, otherTask, runOptions) } doReturn 0
-                        on { run(config, mainTask, runOptions) } doReturn expectedTaskExitCode
-                    }
-
-                    val command = RunTaskCommand(configFile, runOptions, configLoader, taskExecutionOrderResolver, taskRunner, updateNotifier, dockerClient, console, errorConsole, logger)
-                    val exitCode = command.run()
-
-                    it("runs the dependency task") {
-                        verify(taskRunner).run(config, otherTask, runOptions)
-                    }
-
-                    it("runs the main task") {
-                        verify(taskRunner).run(config, mainTask, runOptions)
-                    }
-
-                    it("runs the dependency before the main task, and prints a blank line in between") {
-                        inOrder(taskRunner, console) {
-                            verify(taskRunner).run(config, otherTask, runOptions)
-                            verify(console).println()
-                            verify(taskRunner).run(config, mainTask, runOptions)
-                        }
-                    }
-
-                    it("returns the exit code of the main task") {
-                        assertThat(exitCode, equalTo(expectedTaskExitCode))
-                    }
-
-                    it("displays any update notifications before running the task") {
-                        inOrder(taskRunner, updateNotifier) {
-                            verify(updateNotifier).run()
-                            verify(taskRunner, atLeastOnce()).run(any(), any(), any())
-                        }
-                    }
-
-                    it("does not print anything to the error console") {
-                        verifyZeroInteractions(errorConsole)
-                    }
-                }
-
-                on("and the dependency finishes with a non-zero exit code") {
-                    val taskRunner = mock<TaskRunner> {
-                        on { run(config, otherTask, runOptions) } doReturn 1
-                    }
-
-                    val command = RunTaskCommand(configFile, runOptions, configLoader, taskExecutionOrderResolver, taskRunner, updateNotifier, dockerClient, console, errorConsole, logger)
-                    val exitCode = command.run()
-
-                    it("runs the dependency task") {
-                        verify(taskRunner).run(config, otherTask, runOptions)
-                    }
-
-                    it("does not run the main task") {
-                        verify(taskRunner, never()).run(config, mainTask, runOptions)
-                    }
-
-                    it("returns the exit code of the dependency task") {
-                        assertThat(exitCode, equalTo(1))
-                    }
-
-                    it("displays any update notifications before running the task") {
-                        inOrder(taskRunner, updateNotifier) {
-                            verify(updateNotifier).run()
-                            verify(taskRunner).run(any(), any(), any())
-                        }
-                    }
-
-                    it("does not print anything to the error console") {
-                        verifyZeroInteractions(errorConsole)
-                    }
-                }
-            }
-
-            on("when the determining the task execution order fails") {
-                val exception = TaskExecutionOrderResolutionException("Something went wrong.")
-                val taskExecutionOrderResolver = mock<TaskExecutionOrderResolver> {
-                    on { resolveExecutionOrder(config, taskName) } doThrow exception
-                }
-
-                val taskRunner = mock<TaskRunner>()
-                val dockerClient = mock<DockerClient> {
-                    on { checkIfDockerIsAvailable() } doReturn true
-                }
-
-                val command = RunTaskCommand(configFile, runOptions, configLoader, taskExecutionOrderResolver, taskRunner, updateNotifier, dockerClient, console, errorConsole, logger)
-                val exitCode = command.run()
-
-                it("prints a message to the output") {
-                    verify(redErrorConsole).println("Something went wrong.")
-                }
-
-                it("returns a non-zero exit code") {
-                    assertThat(exitCode, !equalTo(0))
-                }
-
-                it("logs a message with the exception") {
-                    assertThat(logSink, hasMessage(withSeverity(Severity.Error) and withException(exception)))
-                }
-            }
-
-            on("when Docker is not available") {
-                val taskExecutionOrderResolver = mock<TaskExecutionOrderResolver>()
-                val taskRunner = mock<TaskRunner>()
-                val dockerClient = mock<DockerClient> {
-                    on { checkIfDockerIsAvailable() } doReturn false
-                }
-
-                val command = RunTaskCommand(configFile, runOptions, configLoader, taskExecutionOrderResolver, taskRunner, updateNotifier, dockerClient, console, errorConsole, logger)
-                val exitCode = command.run()
-
-                it("prints a message to the output") {
-                    verify(redErrorConsole).println("Docker is not installed, or the Docker executable is not on your PATH.")
-                }
-
-                it("returns a non-zero exit code") {
-                    assertThat(exitCode, !equalTo(0))
                 }
             }
         }

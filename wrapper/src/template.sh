e23b92784f4e5ffa0412a928f3a217cabf7c2f25
Charles Korn <me@charleskorn.com>
2018-05-13 19:03:35 +1000

    Fix the issue where upgrading batect could result in weird errors.
    This happens if the upgrade adds or removes lines from the wrapper
    script, as Bash does not read the entire file into memory before
    running it and so continues execution from the same place in the (now
    modified) script after batect exits.
    
    With this change, Bash is forced to read the entire script into memory
    immediately, preventing weird things from happening.
    
    This won't fix the issue for upgrades to this version, but will fix it
    for subsequent upgrades.

--- wrapper/src/template.sh
+++ wrapper/src/template.sh
@@ -1,51 +1,54 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+{
+    set -euo pipefail
 
-VERSION="VERSION-GOES-HERE"
-DOWNLOAD_URL=${BATECT_DOWNLOAD_URL:-"DOWNLOAD-URL-GOES-HERE"}
+    VERSION="VERSION-GOES-HERE"
+    DOWNLOAD_URL=${BATECT_DOWNLOAD_URL:-"DOWNLOAD-URL-GOES-HERE"}
 
-ROOT_CACHE_DIR=${BATECT_CACHE_DIR:-"$HOME/.batect/cache"}
-CACHE_DIR="$ROOT_CACHE_DIR/$VERSION"
-JAR_PATH="$CACHE_DIR/batect-$VERSION.jar"
+    ROOT_CACHE_DIR=${BATECT_CACHE_DIR:-"$HOME/.batect/cache"}
+    CACHE_DIR="$ROOT_CACHE_DIR/$VERSION"
+    JAR_PATH="$CACHE_DIR/batect-$VERSION.jar"
 
-SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+    SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 
-function main() {
-    if ! haveVersionCachedLocally; then
-        download
-    fi
+    function main() {
+        if ! haveVersionCachedLocally; then
+            download
+        fi
 
-    runApplication "$@"
+        runApplication "$@"
+    }
+
+    function haveVersionCachedLocally() {
+        [ -f "$JAR_PATH" ]
+    }
+
+    function download() {
+        echo "Downloading batect version $VERSION from $DOWNLOAD_URL..."
+
+        if ! hash curl 2>/dev/null; then
+            echo "curl is not installed or not on your PATH. Please install it and try again." >&2
+            exit -1
+        fi
+
+        mkdir -p "$CACHE_DIR"
+        temp_file=$(mktemp)
+        curl -# --fail --show-error --location --output "$temp_file" "$DOWNLOAD_URL"
+        mv "$temp_file" "$JAR_PATH"
+    }
+
+    function runApplication() {
+        if ! hash java 2>/dev/null; then
+            echo "Java is not installed or not on your PATH. Please install it and try again." >&2
+            exit -1
+        fi
+
+        BATECT_WRAPPER_SCRIPT_PATH="$SCRIPT_PATH" \
+        HOSTNAME="$HOSTNAME" \
+        java -Djava.net.useSystemProxies=true -jar "$JAR_PATH" "$@"
+    }
+
+    main "$@"
+    exit $?
 }
-
-function haveVersionCachedLocally() {
-    [ -f "$JAR_PATH" ]
-}
-
-function download() {
-    echo "Downloading batect version $VERSION from $DOWNLOAD_URL..."
-
-    if ! hash curl 2>/dev/null; then
-        echo "curl is not installed or not on your PATH. Please install it and try again." >&2
-        exit -1
-    fi
-
-    mkdir -p "$CACHE_DIR"
-    temp_file=$(mktemp)
-    curl -# --fail --show-error --location --output "$temp_file" "$DOWNLOAD_URL"
-    mv "$temp_file" "$JAR_PATH"
-}
-
-function runApplication() {
-    if ! hash java 2>/dev/null; then
-        echo "Java is not installed or not on your PATH. Please install it and try again." >&2
-        exit -1
-    fi
-
-    BATECT_WRAPPER_SCRIPT_PATH="$SCRIPT_PATH" \
-    HOSTNAME="$HOSTNAME" \
-    java -Djava.net.useSystemProxies=true -jar "$JAR_PATH" "$@"
-}
-
-main "$@"

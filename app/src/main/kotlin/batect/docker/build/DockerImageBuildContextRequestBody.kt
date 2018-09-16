9449f4c26ea67aae07bf5a037a9dc3f9dac4d261
Charles Korn <me@charleskorn.com>
2018-09-16 19:57:45 +1000

    Add class to represent a request body with an image's build context.

---
+++ app/src/main/kotlin/batect/docker/build/DockerImageBuildContextRequestBody.kt
@@ -0,0 +1,70 @@
+/*
+   Copyright 2017-2018 Charles Korn.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package batect.docker.build
+
+import okhttp3.MediaType
+import okhttp3.RequestBody
+import okio.BufferedSink
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream
+import java.nio.file.Files
+import java.nio.file.LinkOption
+
+data class DockerImageBuildContextRequestBody(val context: DockerImageBuildContext) : RequestBody() {
+    override fun contentType(): MediaType = MediaType.get("application/x-tar")
+
+    override fun writeTo(sink: BufferedSink) {
+        TarArchiveOutputStream(sink.outputStream()).use { output ->
+            output.setLongFileMode(TarArchiveOutputStream.LONGFILE_POSIX)
+
+            context.entries.forEach { entry ->
+                writeEntry(entry, output)
+            }
+        }
+    }
+
+    private fun writeEntry(entry: DockerImageBuildContextEntry, output: TarArchiveOutputStream) {
+        val archiveEntry = if (Files.isSymbolicLink(entry.localPath)) {
+            createSymbolicLinkEntry(entry)
+        } else {
+            createNormalEntry(entry)
+        }
+
+        archiveEntry.mode = Files.getAttribute(entry.localPath, "unix:mode", LinkOption.NOFOLLOW_LINKS) as Int
+
+        output.putArchiveEntry(archiveEntry)
+
+        if (Files.isRegularFile(entry.localPath, LinkOption.NOFOLLOW_LINKS)) {
+            entry.localPath.toFile().inputStream().use { reader ->
+                reader.copyTo(output)
+            }
+        }
+
+        output.closeArchiveEntry()
+    }
+
+    private fun createNormalEntry(entry: DockerImageBuildContextEntry) = TarArchiveEntry(entry.localPath.toFile(), entry.contextPath)
+
+    private fun createSymbolicLinkEntry(entry: DockerImageBuildContextEntry): TarArchiveEntry {
+        val archiveEntry = TarArchiveEntry(entry.contextPath, TarArchiveEntry.LF_SYMLINK)
+        val target = Files.readSymbolicLink(entry.localPath)
+
+        archiveEntry.linkName = target.toString()
+
+        return archiveEntry
+    }
+}
---
+++ app/src/test/kotlin/batect/docker/build/DockerImageBuildContextRequestBodySpec.kt
@@ -0,0 +1,325 @@
+/*
+   Copyright 2017-2018 Charles Korn.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package batect.docker.build
+
+import batect.testutils.createForEachTest
+import batect.testutils.equalTo
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.has
+import com.natpryce.hamkrest.hasSize
+import com.natpryce.hamkrest.isEmpty
+import com.natpryce.hamkrest.isEmptyString
+import okhttp3.MediaType
+import okio.Okio
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.nio.charset.Charset
+import java.nio.file.Files
+import java.nio.file.LinkOption
+import java.nio.file.Paths
+import java.nio.file.attribute.PosixFilePermissions
+
+object DockerImageBuildContextRequestBodySpec : Spek({
+    describe("a Docker image build context request body") {
+        on("getting the content type") {
+            val requestBody = DockerImageBuildContextRequestBody(DockerImageBuildContext(emptySet()))
+
+            it("returns the TAR content type") {
+                assertThat(requestBody.contentType(), equalTo(MediaType.get("application/x-tar")))
+            }
+        }
+
+        on("getting the content length") {
+            val requestBody = DockerImageBuildContextRequestBody(DockerImageBuildContext(emptySet()))
+
+            it("returns that the content length is unknown") {
+                assertThat(requestBody.contentLength(), equalTo(-1))
+            }
+        }
+
+        describe("writing the context") {
+            val contextDir by createForEachTest { Files.createTempDirectory("${DockerImageBuildContextRequestBodySpec::class.simpleName}-") }
+
+            afterEachTest { contextDir.toFile().deleteRecursively() }
+
+            given("the build context is empty") {
+                val context = DockerImageBuildContext(emptySet())
+                val requestBody = DockerImageBuildContextRequestBody(context)
+
+                on("writing the request body") {
+                    val outputStream = ByteArrayOutputStream()
+                    val sink = Okio.buffer(Okio.sink(outputStream))
+                    requestBody.writeTo(sink)
+
+                    val entries = readAllTarEntries(outputStream)
+
+                    it("writes nothing to the output stream") {
+                        assertThat(entries, isEmpty)
+                    }
+                }
+            }
+
+            given("the build context contains a single file") {
+                val path by createForEachTest { contextDir.resolve("some-file") }
+
+                beforeEachTest {
+                    Files.write(path, listOf("Some content in the file"))
+                    Files.setPosixFilePermissions(path, PosixFilePermissions.fromString("rw------x"))
+                }
+
+                val context by createForEachTest {
+                    DockerImageBuildContext(setOf(
+                        DockerImageBuildContextEntry(path, "path-inside-context")
+                    ))
+                }
+
+                val requestBody by createForEachTest { DockerImageBuildContextRequestBody(context) }
+
+                on("writing the request body") {
+                    val outputStream = ByteArrayOutputStream()
+                    val sink = Okio.buffer(Okio.sink(outputStream))
+                    requestBody.writeTo(sink)
+
+                    val entries = readAllTarEntries(outputStream)
+
+                    it("writes a single entry to the output stream") {
+                        assertThat(entries, hasSize(equalTo(1)))
+                    }
+
+                    it("uses the context name for the file") {
+                        assertThat(entries[0], has(RequestBodyEntry::name, equalTo("path-inside-context")))
+                    }
+
+                    it("includes the content of the file") {
+                        assertThat(entries[0], has(RequestBodyEntry::content, equalTo("Some content in the file\n")))
+                    }
+
+                    it("includes the permissions on the file") {
+                        assertThat(entries[0], has(RequestBodyEntry::mode, equalTo(Files.getAttribute(path, "unix:mode"))))
+                    }
+
+                    it("marks the file as a file") {
+                        assertThat(entries[0], has(RequestBodyEntry::isFile, equalTo(true)))
+                    }
+
+                    it("does not mark the file as a directory") {
+                        assertThat(entries[0], has(RequestBodyEntry::isDirectory, equalTo(false)))
+                    }
+                }
+            }
+
+            given("the build context contains a single file with a name over 100 characters long") {
+                val path by createForEachTest { contextDir.resolve("some-file") }
+
+                beforeEachTest {
+                    Files.createFile(path)
+                }
+
+                val nameInsideContext = "0123456789".repeat(10) + "-abc"
+
+                val context by createForEachTest {
+                    DockerImageBuildContext(setOf(
+                        DockerImageBuildContextEntry(path, nameInsideContext)
+                    ))
+                }
+
+                val requestBody by createForEachTest { DockerImageBuildContextRequestBody(context) }
+
+                on("writing the request body") {
+                    val outputStream = ByteArrayOutputStream()
+                    val sink = Okio.buffer(Okio.sink(outputStream))
+                    requestBody.writeTo(sink)
+
+                    val entries = readAllTarEntries(outputStream)
+
+                    it("writes a single entry to the output stream") {
+                        assertThat(entries, hasSize(equalTo(1)))
+                    }
+
+                    it("uses the context name for the file") {
+                        assertThat(entries[0], has(RequestBodyEntry::name, equalTo(nameInsideContext)))
+                    }
+                }
+            }
+
+            given("the build context contains a single directory") {
+                val path by createForEachTest { contextDir.resolve("some-directory") }
+
+                beforeEachTest {
+                    Files.createDirectory(path)
+                    Files.setPosixFilePermissions(path, PosixFilePermissions.fromString("rw------x"))
+                }
+
+                val context by createForEachTest {
+                    DockerImageBuildContext(setOf(
+                        DockerImageBuildContextEntry(path, "path-inside-context")
+                    ))
+                }
+
+                val requestBody by createForEachTest { DockerImageBuildContextRequestBody(context) }
+
+                on("writing the request body") {
+                    val outputStream = ByteArrayOutputStream()
+                    val sink = Okio.buffer(Okio.sink(outputStream))
+                    requestBody.writeTo(sink)
+
+                    val entries = readAllTarEntries(outputStream)
+
+                    it("writes a single entry to the output stream") {
+                        assertThat(entries, hasSize(equalTo(1)))
+                    }
+
+                    it("uses the context name for the directory") {
+                        assertThat(entries[0], has(RequestBodyEntry::name, equalTo("path-inside-context/")))
+                    }
+
+                    it("includes the permissions on the directory") {
+                        assertThat(entries[0], has(RequestBodyEntry::mode, equalTo(Files.getAttribute(path, "unix:mode"))))
+                    }
+
+                    it("does not mark the directory as a file") {
+                        assertThat(entries[0], has(RequestBodyEntry::isFile, equalTo(false)))
+                    }
+
+                    it("marks the directory as a directory") {
+                        assertThat(entries[0], has(RequestBodyEntry::isDirectory, equalTo(true)))
+                    }
+                }
+            }
+
+            given("the build context contains a directory with a file inside that directory") {
+                val directoryPath by createForEachTest { contextDir.resolve("some-directory") }
+                val filePath by createForEachTest { directoryPath.resolve("some-file") }
+
+                beforeEachTest {
+                    Files.createDirectory(directoryPath)
+                    Files.write(filePath, listOf("Some content inside the file"))
+                }
+
+                val context by createForEachTest {
+                    DockerImageBuildContext(setOf(
+                        DockerImageBuildContextEntry(directoryPath, "some-dir-context"),
+                        DockerImageBuildContextEntry(filePath, "some-dir-context/some-file-context")
+                    ))
+                }
+
+                val requestBody by createForEachTest { DockerImageBuildContextRequestBody(context) }
+
+                on("writing the request body") {
+                    val outputStream = ByteArrayOutputStream()
+                    val sink = Okio.buffer(Okio.sink(outputStream))
+                    requestBody.writeTo(sink)
+
+                    val entries = readAllTarEntries(outputStream)
+
+                    it("writes both the directory and file to the output stream") {
+                        assertThat(entries.map { it.name }, equalTo(listOf(
+                            "some-dir-context/",
+                            "some-dir-context/some-file-context"
+                        )))
+                    }
+                }
+            }
+
+            given("the build context contains a symlink") {
+                val path by createForEachTest { contextDir.resolve("some-file") }
+
+                beforeEachTest {
+                    Files.createFile(contextDir.resolve("the-target-file"))
+                    Files.createSymbolicLink(path, Paths.get("the-target-file"))
+                    Files.setPosixFilePermissions(path, PosixFilePermissions.fromString("rw------x"))
+                }
+
+                val context by createForEachTest {
+                    DockerImageBuildContext(setOf(
+                        DockerImageBuildContextEntry(path, "file1")
+                    ))
+                }
+
+                val requestBody by createForEachTest { DockerImageBuildContextRequestBody(context) }
+
+                on("writing the request body") {
+                    val outputStream = ByteArrayOutputStream()
+                    val sink = Okio.buffer(Okio.sink(outputStream))
+                    requestBody.writeTo(sink)
+
+                    val entries = readAllTarEntries(outputStream)
+
+                    it("writes a single entry to the output stream") {
+                        assertThat(entries, hasSize(equalTo(1)))
+                    }
+
+                    it("marks the symlink as a symlink") {
+                        assertThat(entries[0].isSymbolicLink, equalTo(true))
+                    }
+
+                    it("does not include any content for the symlink") {
+                        assertThat(entries[0].content, isEmptyString)
+                    }
+
+                    it("links the symlink to the correct file") {
+                        assertThat(entries[0].linkTarget, equalTo("the-target-file"))
+                    }
+
+                    it("includes the permissions on the symlink, not the target file") {
+                        assertThat(entries[0], has(RequestBodyEntry::mode, equalTo(Files.getAttribute(path, "unix:mode", LinkOption.NOFOLLOW_LINKS))))
+                    }
+                }
+            }
+        }
+    }
+})
+
+private data class RequestBodyEntry(
+    val name: String,
+    val mode: Int,
+    val isFile: Boolean,
+    val isDirectory: Boolean,
+    val isSymbolicLink: Boolean,
+    val linkTarget: String,
+    val content: String
+)
+
+private fun readAllTarEntries(outputStream: ByteArrayOutputStream): List<RequestBodyEntry> {
+    val inputStream = ByteArrayInputStream(outputStream.toByteArray())
+    val reader = TarArchiveInputStream(inputStream)
+    val entries = mutableListOf<RequestBodyEntry>()
+
+    while (reader.nextEntry != null) {
+        val entry = reader.currentEntry
+        val contentStream = ByteArrayOutputStream()
+        reader.copyTo(contentStream)
+
+        entries.add(RequestBodyEntry(
+            entry.name,
+            entry.mode,
+            entry.isFile,
+            entry.isDirectory,
+            entry.isSymbolicLink,
+            entry.linkName,
+            contentStream.toByteArray().toString(Charset.defaultCharset())
+        ))
+    }
+
+    return entries
+}

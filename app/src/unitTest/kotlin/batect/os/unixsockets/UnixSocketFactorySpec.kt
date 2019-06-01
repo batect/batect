09d13b6d0afab34598b3ba37b86b8f834404651c
Charles Korn <me@charleskorn.com>
2019-06-01 11:08:00 +1000

    Only run Unix socket tests on platforms that support Unix sockets.

--- app/src/unitTest/kotlin/batect/os/unixsockets/UnixSocketFactorySpec.kt
+++ app/src/unitTest/kotlin/batect/os/unixsockets/UnixSocketFactorySpec.kt
@@ -19,11 +19,13 @@ package batect.os.unixsockets
 import batect.testutils.createForEachTest
 import batect.testutils.equalTo
 import batect.testutils.on
+import batect.testutils.onlyOn
 import batect.testutils.runForEachTest
 import batect.testutils.withMessage
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.throws
 import jnr.enxio.channels.NativeSelectorProvider
+import jnr.ffi.Platform
 import jnr.unixsocket.UnixServerSocketChannel
 import jnr.unixsocket.UnixSocketAddress
 import org.spekframework.spek2.Spek
@@ -38,63 +40,81 @@ import java.nio.file.Path
 import kotlin.concurrent.thread
 
 object UnixSocketFactorySpec : Spek({
-    describe("a Unix socket factory") {
-        val factory = UnixSocketFactory()
+    onlyOn(setOf(Platform.OS.LINUX, Platform.OS.DARWIN)) {
+        describe("a Unix socket factory") {
+            val factory = UnixSocketFactory()
 
-        describe("creating a socket") {
-            val socket by createForEachTest { factory.createSocket() }
+            describe("creating a socket") {
+                val socket by createForEachTest { factory.createSocket() }
 
-            afterEachTest { socket.close() }
+                afterEachTest { socket.close() }
 
-            on("using that socket to connect to a Unix socket") {
-                val dataRead by runForEachTest {
-                    val socketPath = getTemporarySocketFileName()
-                    val serverChannel = createSocketServer(socketPath)
+                on("using that socket to connect to a Unix socket") {
+                    val dataRead by runForEachTest {
+                        val socketPath = getTemporarySocketFileName()
+                        val serverChannel = createSocketServer(socketPath)
 
-                    val port = 1234
-                    val encodedPath = UnixSocketDns.encodePath(socketPath.toString())
-                    val address = InetSocketAddress.createUnresolved(encodedPath, port)
-                    socket.connect(address)
-                    socket.soTimeout = 1000
+                        val port = 1234
+                        val encodedPath = UnixSocketDns.encodePath(socketPath.toString())
+                        val address = InetSocketAddress.createUnresolved(encodedPath, port)
+                        socket.connect(address)
+                        socket.soTimeout = 1000
 
-                    val dataRead = socket.getInputStream().bufferedReader().readLine()
-                    serverChannel.close()
+                        val dataRead = socket.getInputStream().bufferedReader().readLine()
+                        serverChannel.close()
 
-                    dataRead
+                        dataRead
+                    }
+
+                    it("connects to the socket and can receive data") {
+                        assertThat(dataRead, equalTo("Hello from the other side"))
+                    }
                 }
 
-                it("connects to the socket and can receive data") {
-                    assertThat(dataRead, equalTo("Hello from the other side"))
+                on("using that socket to connect to a Unix socket that doesn't exist") {
+                    val encodedPath = UnixSocketDns.encodePath("/var/run/does-not-exist.sock")
+                    val address = InetSocketAddress.createUnresolved(encodedPath, 1234)
+
+                    it("throws an appropriate exception") {
+                        assertThat(
+                            { socket.connect(address) },
+                            throws<IOException>(withMessage("Cannot connect to '/var/run/does-not-exist.sock': No such file or directory"))
+                        )
+                    }
                 }
             }
 
-            on("using that socket to connect to a Unix socket that doesn't exist") {
-                val encodedPath = UnixSocketDns.encodePath("/var/run/does-not-exist.sock")
-                val address = InetSocketAddress.createUnresolved(encodedPath, 1234)
+            on("creating a socket with a particular host and port") {
+                it("throws an appropriate exception when using primitive values") {
+                    assertThat({ factory.createSocket("somehost", 123) }, throws<UnsupportedOperationException>())
+                }
 
-                it("throws an appropriate exception") {
-                    assertThat({ socket.connect(address) }, throws<IOException>(withMessage("Cannot connect to '/var/run/does-not-exist.sock': No such file or directory")))
+                it("throws an appropriate exception when using non-primitive values") {
+                    assertThat(
+                        { factory.createSocket(InetAddress.getLocalHost(), 123) },
+                        throws<UnsupportedOperationException>()
+                    )
                 }
             }
-        }
 
-        on("creating a socket with a particular host and port") {
-            it("throws an appropriate exception when using primitive values") {
-                assertThat({ factory.createSocket("somehost", 123) }, throws<UnsupportedOperationException>())
-            }
+            on("creating a socket with both a local and remote host and port") {
+                it("throws an appropriate exception when using primitive values") {
+                    assertThat(
+                        { factory.createSocket("somehost", 123, InetAddress.getLocalHost(), 123) },
+                        throws<UnsupportedOperationException>()
+                    )
+                }
 
-            it("throws an appropriate exception when using non-primitive values") {
-                assertThat({ factory.createSocket(InetAddress.getLocalHost(), 123) }, throws<UnsupportedOperationException>())
-            }
-        }
-
-        on("creating a socket with both a local and remote host and port") {
-            it("throws an appropriate exception when using primitive values") {
-                assertThat({ factory.createSocket("somehost", 123, InetAddress.getLocalHost(), 123) }, throws<UnsupportedOperationException>())
-            }
-
-            it("throws an appropriate exception when using non-primitive values") {
-                assertThat({ factory.createSocket(InetAddress.getLocalHost(), 123, InetAddress.getLocalHost(), 123) }, throws<UnsupportedOperationException>())
+                it("throws an appropriate exception when using non-primitive values") {
+                    assertThat({
+                        factory.createSocket(
+                            InetAddress.getLocalHost(),
+                            123,
+                            InetAddress.getLocalHost(),
+                            123
+                        )
+                    }, throws<UnsupportedOperationException>())
+                }
             }
         }
     }

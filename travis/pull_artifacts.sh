a7ff94ba0e3e3bcbe043b6cc6944e060acda9cdf
Charles Korn <me@charleskorn.com>
2019-07-20 20:22:43 +1000

    Run integration and journey testing in parallel, and only setup GCP credentials when needed.
    This should improve the performance of the build.

--- travis/pull_artifacts.sh
+++ travis/pull_artifacts.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+"$SCRIPT_PATH/setup_creds.sh"
 mkdir -p build/bintray build/release docs/build/docs
 gsutil -m rsync -r gs://batect-artifacts/$TRAVIS_BUILD_NUMBER/build/bintray build/bintray
 gsutil -m rsync -r gs://batect-artifacts/$TRAVIS_BUILD_NUMBER/build/release build/release

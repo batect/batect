e890dbbec6c320acf0e8c138f8daeb681cec4f1b
Charles Korn <me@charleskorn.com>
2019-07-21 19:57:31 +1000

    Wait for the AppVeyor build to complete before publishing any build artifacts.

---
+++ travis/wait_for_appveyor.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+REPO_SLUG=$1
+GIT_COMMIT=$2
+GIT_BRANCH=$3
+
+function main() {
+    URL="https://ci.appveyor.com/api/projects/$REPO_SLUG/history?recordsNumber=20&branch=$GIT_BRANCH"
+
+    echo "Will poll $URL."
+
+    while true; do
+        status=$(curl --fail --show-error --silent "$URL" | jq -r ".builds | .[] | select(.commitId == \"$GIT_COMMIT\").status")
+
+        case "$status" in
+        success)
+            echo "Build succeeded."
+            exit 0
+            ;;
+        failed)
+            echo "Build failed."
+            exit 1
+            ;;
+        "")
+            echo "Build has not started."
+            ;;
+        *)
+            echo "Build has not completed: status is $status"
+            ;;
+        esac
+
+        sleep 1
+    done
+}
+
+main

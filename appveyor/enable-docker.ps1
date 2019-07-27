cdbfeccafdf8397dcca98a87f3b9b41e1e785ffc
Charles Korn <me@charleskorn.com>
2019-07-27 17:42:22 +1000

    Use the current user to mount directories into Docker containers during the Windows build.

--- appveyor/enable-docker.ps1
+++ appveyor/enable-docker.ps1
@@ -1,10 +1,9 @@
 # Based on https://github.com/appveyor/ci/blob/master/scripts/docker-appveyor.psm1, adapted to use C: instead of D:
 
 Remove-SmbShare -Name C -ErrorAction SilentlyContinue -Force
-$deUsername = 'DockerExchange'
-$dePsw = "ABC" + [guid]::NewGuid().ToString() + "!"
-$secDePsw = ConvertTo-SecureString $dePsw -AsPlainText -Force
-Get-LocalUser -Name $deUsername | Set-LocalUser -Password $secDePsw
+$password = "ABC" + [guid]::NewGuid().ToString() + "!"
+$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+Get-LocalUser -Name $env:UserName | Set-LocalUser -Password $securePassword
 & $env:ProgramFiles\Docker\Docker\DockerCli.exe -Start --testftw!928374kasljf039 >$null 2>&1
-& $env:ProgramFiles\Docker\Docker\DockerCli.exe -Mount=C -Username="$env:computername\$deUsername" -Password="$dePsw" --testftw!928374kasljf039 >$null 2>&1
+& $env:ProgramFiles\Docker\Docker\DockerCli.exe -Mount=C -Username="$env:computername\$env:UserName" -Password="$password" --testftw!928374kasljf039 >$null 2>&1
 Disable-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Direction Inbound

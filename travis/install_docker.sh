2320544572b8894fd35038b183c90c0453cb2009
Charles Korn <me@charleskorn.com>
2019-07-28 17:00:33 +1000

    Fix 'hash sum mismatch' errors while installing Docker on Travis.

--- travis/install_docker.sh
+++ travis/install_docker.sh
@@ -4,6 +4,10 @@ set -euo pipefail
 
 PACKAGE=${1:-docker-ce}
 
+# This works around 'hash sum mismatch' issues.
+# See https://blog.packagecloud.io/eng/2016/03/21/apt-hash-sum-mismatch/ for more details.
+echo 'Acquire::CompressionTypes::Order:: "gz";' > /etc/apt/apt.conf.d/99compression-workaround
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update

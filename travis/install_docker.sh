e8ae25ceadc8051e53b54a5ecc0b382d1ed05e36
Charles Korn <me@charleskorn.com>
2019-07-28 17:14:30 +1000

    Modify Apt configuration file as root.

--- travis/install_docker.sh
+++ travis/install_docker.sh
@@ -6,7 +6,7 @@ PACKAGE=${1:-docker-ce}
 
 # This works around 'hash sum mismatch' issues.
 # See https://blog.packagecloud.io/eng/2016/03/21/apt-hash-sum-mismatch/ for more details.
-echo 'Acquire::CompressionTypes::Order:: "gz";' > /etc/apt/apt.conf.d/99compression-workaround
+echo 'Acquire::CompressionTypes::Order:: "gz";' | sudo tee /etc/apt/apt.conf.d/99compression-workaround
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"

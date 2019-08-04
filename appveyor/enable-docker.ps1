97381a52717a563c99c6ea1b832a351b7f3ea4ee
Charles Korn <me@charleskorn.com>
2019-08-04 10:29:48 +1000

    Use new custom Azure build environment for Windows builds.

--- appveyor/enable-docker.ps1
+++ appveyor/enable-docker.ps1
@@ -4,6 +4,10 @@ Remove-SmbShare -Name C -ErrorAction SilentlyContinue -Force
 $password = "ABC" + [guid]::NewGuid().ToString() + "!"
 $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
 Get-LocalUser -Name $env:UserName | Set-LocalUser -Password $securePassword
+
+Write-Output "Starting Docker..."
 & $env:ProgramFiles\Docker\Docker\DockerCli.exe -Start --testftw!928374kasljf039 >$null 2>&1
+
+Write-Output "Configuring sharing..."
 & $env:ProgramFiles\Docker\Docker\DockerCli.exe -Mount=C -Username="$env:computername\$env:UserName" -Password="$password" --testftw!928374kasljf039 >$null 2>&1
 Disable-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Direction Inbound

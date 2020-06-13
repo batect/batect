4febdae0f7d50bbdf9c4a380d92dec6c624a3095
Charles Korn <me@charleskorn.com>
2020-06-13 22:51:49 +1000

    Make TaskEvent sealed.

--- app/src/main/kotlin/batect/execution/model/events/TaskEvent.kt
+++ app/src/main/kotlin/batect/execution/model/events/TaskEvent.kt
@@ -16,4 +16,171 @@
 
 package batect.execution.model.events
 
-abstract class TaskEvent(val isInformationalEvent: Boolean = false)
+import batect.config.Container
+import batect.config.PullImage
+import batect.config.SetupCommand
+import batect.docker.DockerContainer
+import batect.docker.DockerImage
+import batect.docker.DockerNetwork
+import batect.docker.client.DockerImageBuildProgress
+import batect.docker.pull.DockerImageProgress
+import batect.execution.model.steps.TaskStep
+import java.nio.file.Path
+
+sealed class TaskEvent(val isInformationalEvent: Boolean = false)
+
+object CachesInitialisedEvent : TaskEvent() {
+    override fun toString() = this::class.simpleName!!
+}
+
+data class ContainerBecameHealthyEvent(val container: Container) : TaskEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}')"
+}
+
+data class ContainerBecameReadyEvent(val container: Container) : TaskEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}')"
+}
+
+data class ContainerCreatedEvent(val container: Container, val dockerContainer: DockerContainer) : TaskEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}', Docker container ID: '${dockerContainer.id}')"
+}
+
+data class ContainerRemovedEvent(val container: Container) : TaskEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}')"
+}
+
+data class ContainerStartedEvent(val container: Container) : TaskEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}')"
+}
+
+data class ContainerStoppedEvent(val container: Container) : TaskEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}')"
+}
+
+data class ImageBuildProgressEvent(val container: Container, val buildProgress: DockerImageBuildProgress) : TaskEvent(isInformationalEvent = true) {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}', current step: ${buildProgress.currentStep}, total steps: ${buildProgress.totalSteps}, message: '${buildProgress.message}', pull progress: ${formatPullProgress()})"
+
+    private fun formatPullProgress() = if (buildProgress.progress == null) {
+        "null"
+    } else {
+        "'" + buildProgress.progress.toStringForDisplay() + "'"
+    }
+}
+
+data class ImageBuiltEvent(val container: Container, val image: DockerImage) : TaskEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}', image: '${image.id}')"
+}
+
+data class ImagePulledEvent(val source: PullImage, val image: DockerImage) : TaskEvent() {
+    override fun toString() = "${this::class.simpleName}(source: $source, image: '${image.id}')"
+}
+
+data class ImagePullProgressEvent(val source: PullImage, val progress: DockerImageProgress) : TaskEvent(isInformationalEvent = true) {
+    override fun toString() = "${this::class.simpleName}(source: $source, current operation: '${progress.currentOperation}', completed bytes: ${progress.completedBytes}, total bytes: ${progress.totalBytes})"
+}
+
+data class RunningContainerExitedEvent(val container: Container, val exitCode: Long) : TaskEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}', exit code: $exitCode)"
+}
+
+data class RunningSetupCommandEvent(val container: Container, val command: SetupCommand, val commandIndex: Int) : TaskEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}', command: $command, command index: $commandIndex)"
+}
+
+data class SetupCommandsCompletedEvent(val container: Container) : TaskEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}')"
+}
+
+data class StepStartingEvent(val step: TaskStep) : TaskEvent(true) {
+    override fun toString() = "${this::class.simpleName}(step: $step)"
+}
+
+data class TaskNetworkCreatedEvent(val network: DockerNetwork) : TaskEvent() {
+    override fun toString() = "${this::class.simpleName}(network ID: '${network.id}')"
+}
+
+object TaskNetworkDeletedEvent : TaskEvent() {
+    override fun toString() = this::class.simpleName!!
+}
+
+data class TemporaryDirectoryCreatedEvent(val container: Container, val directoryPath: Path) : TaskEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}', directory path: '$directoryPath')"
+}
+
+data class TemporaryDirectoryDeletedEvent(val directoryPath: Path) : TaskEvent() {
+    override fun toString() = "${this::class.simpleName}(directory path: '$directoryPath')"
+}
+
+data class TemporaryFileCreatedEvent(val container: Container, val filePath: Path) : TaskEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}', file path: '$filePath')"
+}
+
+data class TemporaryFileDeletedEvent(val filePath: Path) : TaskEvent() {
+    override fun toString() = "${this::class.simpleName}(file path: '$filePath')"
+}
+
+sealed class TaskFailedEvent : TaskEvent()
+
+data class ExecutionFailedEvent(val message: String) : TaskFailedEvent() {
+    override fun toString() = "${this::class.simpleName}(message: '$message')"
+}
+
+data class TaskNetworkCreationFailedEvent(val message: String) : TaskFailedEvent() {
+    override fun toString() = "${this::class.simpleName}(message: '$message')"
+}
+
+data class ImageBuildFailedEvent(val container: Container, val message: String) : TaskFailedEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}', message: '$message')"
+}
+
+data class ImagePullFailedEvent(val source: PullImage, val message: String) : TaskFailedEvent() {
+    override fun toString() = "${this::class.simpleName}(source: $source, message: '$message')"
+}
+
+data class ContainerCreationFailedEvent(val container: Container, val message: String) : TaskFailedEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}', message: '$message')"
+}
+
+data class ContainerDidNotBecomeHealthyEvent(val container: Container, val message: String) : TaskFailedEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}', message: '$message')"
+}
+
+data class ContainerRunFailedEvent(val container: Container, val message: String) : TaskFailedEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}', message: '$message')"
+}
+
+data class ContainerStopFailedEvent(val container: Container, val message: String) : TaskFailedEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}', message: '$message')"
+}
+
+data class ContainerRemovalFailedEvent(val container: Container, val message: String) : TaskFailedEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}', message: '$message')"
+}
+
+data class TaskNetworkDeletionFailedEvent(val message: String) : TaskFailedEvent() {
+    override fun toString() = "${this::class.simpleName}(message: '$message')"
+}
+
+data class TemporaryFileDeletionFailedEvent(val filePath: Path, val message: String) : TaskFailedEvent() {
+    override fun toString() = "${this::class.simpleName}(file path: '$filePath', message: '$message')"
+}
+
+data class TemporaryDirectoryDeletionFailedEvent(val directoryPath: Path, val message: String) : TaskFailedEvent() {
+    override fun toString() = "${this::class.simpleName}(directory path: '$directoryPath', message: '$message')"
+}
+
+object UserInterruptedExecutionEvent : TaskFailedEvent() {
+    override fun toString() = this::class.simpleName!!
+}
+
+data class SetupCommandExecutionErrorEvent(val container: Container, val command: SetupCommand, val message: String) : TaskFailedEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}', command: $command, message: '$message')"
+}
+
+data class SetupCommandFailedEvent(val container: Container, val command: SetupCommand, val exitCode: Int, val output: String) : TaskFailedEvent() {
+    override fun toString() = "${this::class.simpleName}(container: '${container.name}', command: $command, exit code: $exitCode, output: '$output')"
+}
+
+data class CacheInitialisationFailedEvent(val message: String) : TaskFailedEvent() {
+    override fun toString() = "${this::class.simpleName}(message: '$message')"
+}

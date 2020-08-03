0dba5d389813881436313f15e5bf2a6ffbf0e71b
Charles Korn <me@charleskorn.com>
2020-08-03 12:25:56 +1000

    Update to latest version of mkdocs-material.

--- docs/build.sh
+++ docs/build.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-mkdocs build
+mkdocs build --strict
 
 # Work around the fact that we run the build from within a Docker container (without using batect)
 chmod -R a+rwx build/docs

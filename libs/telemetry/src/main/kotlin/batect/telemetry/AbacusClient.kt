7ed92db9fcf619b42400535957e3b0a7538bae71
Charles Korn <me@charleskorn.com>
2020-08-09 13:55:33 +1000

    Add telemetry API client.

---
+++ libs/telemetry/src/main/kotlin/batect/telemetry/AbacusClient.kt
@@ -0,0 +1,63 @@
+/*
+   Copyright 2017-2020 Charles Korn.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package batect.telemetry
+
+import batect.logging.Logger
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+
+class AbacusClient(
+    private val client: OkHttpClient,
+    private val logger: Logger
+) {
+    private val host = System.getenv().getOrDefault("BATECT_TELEMETRY_HOST", "api.abacus.batect.dev")
+    private val url = "https://$host/v1/sessions"
+    private val jsonMediaType = "application/json".toMediaType()
+
+    fun upload(sessionJson: ByteArray) {
+        val request = Request.Builder()
+            .method("PUT", sessionJson.toRequestBody(jsonMediaType))
+            .url(url)
+            .build()
+
+        logger.info {
+            message("Uploading session.")
+            data("url", url)
+        }
+
+        try {
+            client.newCall(request).execute().use { response ->
+                logger.info {
+                    message("Finished uploading session.")
+                    data("successful", response.isSuccessful)
+                    data("httpResponseCode", response.code)
+                    data("httpResponseMessage", response.message)
+                }
+
+                if (!response.isSuccessful && response.code != 304) {
+                    throw AbacusClientException("The server returned HTTP ${response.code}.")
+                }
+            }
+        } catch (e: Throwable) {
+            throw AbacusClientException("HTTP PUT $url failed: ${e.message}", e)
+        }
+    }
+}
+
+class AbacusClientException(message: String, cause: Throwable? = null) : RuntimeException(message, cause)
---
+++ libs/telemetry/src/unitTest/kotlin/batect/telemetry/AbacusClientSpec.kt
@@ -0,0 +1,108 @@
+/*
+   Copyright 2017-2020 Charles Korn.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package batect.telemetry
+
+import batect.testutils.createForEachTest
+import batect.testutils.given
+import batect.testutils.logging.createLoggerForEachTestWithoutCustomSerializers
+import batect.testutils.mock
+import batect.testutils.withCause
+import batect.testutils.withMessage
+import com.natpryce.hamkrest.and
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import com.natpryce.hamkrest.throws
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.argThat
+import com.nhaarman.mockitokotlin2.doThrow
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import java.io.IOException
+import okhttp3.Call
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okio.Buffer
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+object AbacusClientSpec : Spek({
+    describe("an Abacus client") {
+        val httpClient by createForEachTest { mock<OkHttpClient>() }
+        val logger by createLoggerForEachTestWithoutCustomSerializers()
+        val client by createForEachTest { AbacusClient(httpClient, logger) }
+
+        describe("uploading a session") {
+            val uploadUrl = "https://api.abacus.batect.dev/v1/sessions"
+            val session = "the session as json"
+            val sessionBytes = session.toByteArray(Charsets.UTF_8)
+
+            setOf(200, 304).forEach { statusCode ->
+                given("the service returns a HTTP $statusCode response") {
+                    beforeEachTest {
+                        httpClient.mock("PUT", uploadUrl, "", statusCode)
+
+                        client.upload(sessionBytes)
+                    }
+
+                    it("sets the content type to JSON") {
+                        verify(httpClient).newCall(argThat { body!!.contentType().toString() == "application/json" })
+                    }
+
+                    it("uploads the provided session in the request body") {
+                        verify(httpClient).newCall(requestWithBody(session))
+                    }
+                }
+            }
+
+            given("the service returns a non-successful response code") {
+                beforeEachTest {
+                    httpClient.mock("PUT", uploadUrl, "", 418)
+                }
+
+                it("throws an appropriate exception") {
+                    assertThat({ client.upload(sessionBytes) }, throws<AbacusClientException>(withMessage("HTTP PUT $uploadUrl failed: The server returned HTTP 418.")))
+                }
+            }
+
+            given("the call to the service fails with an exception") {
+                val exception = IOException("Something went wrong.")
+
+                beforeEachTest {
+                    whenever(httpClient.newCall(any())).then {
+                        mock<Call> {
+                            on { execute() } doThrow exception
+                        }
+                    }
+                }
+
+                it("throws an appropriate exception") {
+                    assertThat({ client.upload(sessionBytes) }, throws<AbacusClientException>(
+                        withMessage("HTTP PUT $uploadUrl failed: Something went wrong.")
+                            and withCause(exception)
+                    ))
+                }
+            }
+        }
+    }
+})
+
+internal fun requestWithBody(expectedBody: String) = com.nhaarman.mockitokotlin2.check<Request> { request ->
+    val buffer = Buffer()
+    request.body!!.writeTo(buffer)
+    assertThat(buffer.readString(Charsets.UTF_8), equalTo(expectedBody))
+}

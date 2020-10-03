21c9f87700ecbb626621930e4ce293ee60999765
Charles Korn <me@charleskorn.com>
2020-10-03 20:50:22 +1000

    Silence noisy output from Docker installation script.

--- ci/install_docker.sh
+++ ci/install_docker.sh
@@ -12,7 +12,7 @@ fi
 
 # This works around 'hash sum mismatch' issues.
 # See https://blog.packagecloud.io/eng/2016/03/21/apt-hash-sum-mismatch/ for more details.
-echo 'Acquire::CompressionTypes::Order:: "gz";' | sudo tee /etc/apt/apt.conf.d/99compression-workaround
+echo 'Acquire::CompressionTypes::Order:: "gz";' | sudo tee /etc/apt/apt.conf.d/99compression-workaround > /dev/null
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"

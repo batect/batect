b2e0a7190fe63f83533c1e6db090d348ebd73870
Charles Korn <me@charleskorn.com>
2020-12-06 16:38:13 +1100

    Remove old docs site.
    These are now in https://github.com/batect/batect.dev.

--- docs/build.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env sh
-
-set -euo pipefail
-
-mkdocs build --strict
-
-# Work around the fact that we run the build from within a Docker container (without using batect)
-chmod -R a+rwx build/docs
--- docs/content/tools/Docker.md
+++
@@ -1,5 +0,0 @@
-# Docker
-
-## Building and pushing an image
-
-You can see an example of building and pushing an image from batect in the [Java sample project](https://github.com/batect/batect-sample-java).

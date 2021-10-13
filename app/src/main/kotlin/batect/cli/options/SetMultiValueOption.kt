58157c4438fa2aa28c7f9ee942f0395adbbdbd67
svishal9 <shivendra.vishal@gmail.com>
2021-10-13 17:22:56 +1100

    [1010][Vishal] : Added a new convertor for list of string as per PR review https://github.com/batect/batect/pull/1055#pullrequestreview-773538151.

---
+++ app/src/main/kotlin/batect/cli/options/SetMultiValueOption.kt
@@ -0,0 +1,77 @@
+/*
+    Copyright 2017-2021 Charles Korn.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+package batect.cli.options
+
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+class SetMultiValueOption(
+    group: OptionGroup,
+    longName: String,
+    description: String,
+    shortName: Char? = null,
+    override val valueFormatForHelp: String = "<name>=<value1,value2,value3>"
+) : OptionDefinition(group, longName, description, true, shortName, true), ReadOnlyProperty<OptionParserContainer, List<String>> {
+    private val values: MutableList<String> = mutableListOf()
+    override var valueSource: OptionValueSource = OptionValueSource.Default
+        private set
+
+    override fun parseValue(args: Iterable<String>): OptionParsingResult {
+        val arg = args.first()
+        val argName = arg.substringBefore("=")
+        val useNextArgumentForValue = !arg.contains("=")
+
+        val argValue = if (useNextArgumentForValue) {
+            if (args.count() == 1) return OptionParsingResult.InvalidOption("Option '$arg' requires a value to be provided, either in the form '$argName=<value1, value2, value3, ...>' or '$argName <value1, value2, value3, ...>'.")
+            args.elementAt(1)
+        } else {
+            val value = arg.drop(2).substringAfter("=", "")
+            if (value == "") return OptionParsingResult.InvalidOption("Option '$arg' is in an invalid format, you must provide a value after '='.")
+            value
+        }
+
+        val match = argValue.split(",")
+
+        if (match.isEmpty()) {
+            return OptionParsingResult.InvalidOption("Option '$argName' requires a value to be provided, either in the form '$argName=<value1, value2, value3 ...>' or '$argName <value1, value2, value3 ...>'.")
+        }
+
+        if (match.distinct().size != match.size) {
+            return OptionParsingResult.InvalidOption("Option '$argName' does not allow duplicate values in the list.")
+        }
+
+        values.addAll(match)
+
+        valueSource = OptionValueSource.CommandLine
+
+        return if (useNextArgumentForValue) {
+            OptionParsingResult.ReadOption(2)
+        } else {
+            OptionParsingResult.ReadOption(1)
+        }
+    }
+
+    operator fun provideDelegate(thisRef: OptionParserContainer, property: KProperty<*>): SetMultiValueOption {
+        thisRef.optionParser.addOption(this)
+        return this
+    }
+
+    override fun getValue(thisRef: OptionParserContainer, property: KProperty<*>): List<String> = values
+    override fun checkDefaultValue(): DefaultApplicationResult = DefaultApplicationResult.Succeeded
+    override val descriptionForHelp: String = "${super.descriptionForHelp} Can be provided multiple values."
+
+}
---
+++ app/src/unitTest/kotlin/batect/cli/options/SetMultiValueOptionSpec.kt
@@ -0,0 +1,116 @@
+/*
+    Copyright 2017-2021 Charles Korn.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+package batect.cli.options
+
+import batect.testutils.createForEachTest
+import batect.testutils.equalTo
+import batect.testutils.given
+import batect.testutils.on
+import batect.testutils.runForEachTest
+import com.natpryce.hamkrest.assertion.assertThat
+import org.mockito.kotlin.mock
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+object SetMultiValueOptionSpec : Spek({
+    describe("a list value option") {
+        val group = OptionGroup("the group")
+
+        describe("parsing") {
+            given("an option with short and long names") {
+                val option by createForEachTest { SetMultiValueOption(group, "option", "Some option.", 'o') }
+
+                listOf("--option", "-o").forEach { format ->
+                    on("parsing a list of arguments where the option is specified in the form '$format value'") {
+                        val result by runForEachTest { option.parse(listOf(format, "value", "do-stuff")) }
+
+                        it("indicates that parsing succeeded and that two arguments were consumed") {
+                            assertThat(result, equalTo(OptionParsingResult.ReadOption(2)))
+                        }
+
+                        it("sets the option's value") {
+                            assertThat(option.getValue(mock(), mock()), equalTo(listOf("value")))
+                        }
+
+                        it("reports that the value is from the command line") {
+                            assertThat(option.valueSource, equalTo(OptionValueSource.CommandLine))
+                        }
+                    }
+
+                    on("parsing a list of arguments where the option is specified in the form '$format=value'") {
+                        val result by runForEachTest { option.parse(listOf("$format=value", "do-stuff")) }
+
+                        it("indicates that parsing succeeded and that one argument was consumed") {
+                            assertThat(result, equalTo(OptionParsingResult.ReadOption(1)))
+                        }
+
+                        it("sets the option's value") {
+                            assertThat(option.getValue(mock(), mock()), equalTo(listOf("value")))
+                        }
+
+                        it("reports that the value is from the command line") {
+                            assertThat(option.valueSource, equalTo(OptionValueSource.CommandLine))
+                        }
+                    }
+
+
+                    on("parsing a list of arguments where the option is given in the form '$format=(value)' but no value is provided after the equals sign") {
+                        val result by runForEachTest { option.parse(listOf("$format=", "do-stuff")) }
+
+                        it("indicates that parsing failed") {
+                            assertThat(result, equalTo(OptionParsingResult.InvalidOption("Option '$format=' is in an invalid format, you must provide a value after '='.")))
+                        }
+                    }
+
+                    on("parsing a list of arguments where the option is given in the form '$format (value)' but no second argument is provided") {
+                        val result by runForEachTest { option.parse(listOf(format)) }
+
+                        it("indicates that parsing failed") {
+                            assertThat(result, equalTo(OptionParsingResult.InvalidOption("Option '$format' requires a value to be provided, either in the form '$format=<value1, value2, value3, ...>' or '$format <value1, value2, value3, ...>'.")))
+                        }
+                    }
+
+                    given("a list of options where a value has been duplicated") {
+                        on("parsing a list of arguments where the option is given a second time with the duplicate value") {
+                            val result by runForEachTest { option.parse(listOf(format, "value1,value2,value3,value1")) }
+
+                            it("indicates that parsing failed") {
+                                assertThat(result, equalTo(OptionParsingResult.InvalidOption("Option '$format' does not allow duplicate values in the list.")))
+                            }
+                        }
+                    }
+                }
+            }
+
+            on("not applying a value for the option") {
+                val option = SetMultiValueOption(group, "option", "Some option.")
+
+                it("returns an empty list") {
+                    assertThat(option.getValue(mock(), mock()), equalTo(emptyList()))
+                }
+            }
+        }
+
+        describe("getting the help description for an option") {
+            val option = SetMultiValueOption(group, "option", "Some option.")
+
+            it("returns the provided description") {
+                assertThat(option.descriptionForHelp, equalTo("Some option. Can be provided multiple values."))
+            }
+        }
+    }
+})

f2cf5cac0e19d45a4f2963b8e23cab6da1f67a9d
Charles Korn <me@charleskorn.com>
2022-04-13 14:47:15 +1000

    Publish checksums with each release.

--- wrapper/build.gradle.kts
+++ wrapper/build.gradle.kts
@@ -14,11 +14,58 @@
     limitations under the License.
 */
 
+import org.apache.commons.codec.digest.DigestUtils
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath(libs.commons.codec)
+    }
+}
+
+evaluationDependsOnChildren()
+
+val sourceTasks = setOf(
+    project("unix").tasks.named("build"),
+    project("windows").tasks.named("build")
+)
+
+val createChecksumsFile by tasks.registering {
+    val sourceFiles = sourceTasks.map { it.map { task -> task.outputs } }
+
+    inputs.files(sourceFiles)
+
+    val outputDir = buildDir.resolve("checksums")
+    val outputFile = outputDir.resolve("checksums.sha256")
+
+    outputs.files(outputFile)
+
+    doLast {
+        outputDir.mkdirs()
+
+        val content = sourceFiles
+            .map { it.get() }
+            .flatMap { it.files.files }
+            .associate { file -> file.name to sha256HashOf(file) }
+            .entries
+            .sortedBy { it.key }.joinToString("\n") { "${it.key}  ${it.value}" }
+
+        outputFile.writeText(content, Charsets.UTF_8)
+    }
+}
+
+fun sha256HashOf(file: File): String {
+    return DigestUtils.sha256Hex(file.readBytes())
+}
+
 tasks.register<Copy>("build") {
     description = "Build the wrapper scripts."
     group = "Build"
 
-    from(project("unix").getTasksByName("build", false))
-    from(project("windows").getTasksByName("build", false))
+    from(sourceTasks)
+    from(createChecksumsFile)
     into(buildDir.resolve("build"))
 }
